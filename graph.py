d7ce2678e1f90f5051a27df281f7634246437590
Roundabout1 <mrsivannap@gmail.com>
2024-01-30 14:26:20 +0800

    Update graph.py

--- graph.py
+++ graph.py
@@ -7,20 +7,22 @@ with open('results/train_results.txt', 'r') as f:
     train_data = f.readlines()
 
 # Извлечение точности и функции потерь из данных
-test_accuracy = [float(line.split()[0]) for line in test_data]
-test_loss = [float(line.split()[1]) for line in test_data]
-train_accuracy = [float(line.split()[0]) for line in train_data]
-train_loss = [float(line.split()[1]) for line in train_data]
+test_epochs = [float(line.split()[0]) for line in test_data]
+test_accuracy = [float(line.split()[1]) for line in test_data]
+test_loss = [float(line.split()[2]) for line in test_data]
+train_epochs = [float(line.split()[0]) for line in train_data]
+train_accuracy = [float(line.split()[1]) for line in train_data]
+train_loss = [float(line.split()[2]) for line in train_data]
 
 # Построение графиков
 fig, axs = plt.subplots(2)
 fig.suptitle('Точность и функция потерь за каждую эпоху обучения нейронной сети')
-axs[0].plot(test_accuracy, label='Тестовая выборка')
-axs[0].plot(train_accuracy, label='Обучающая выборка')
+axs[0].plot(test_epochs, test_accuracy, label='Тестовая выборка')
+axs[0].plot(train_epochs, train_accuracy, label='Обучающая выборка')
 axs[0].set_ylabel('Точность')
 axs[0].legend()
-axs[1].plot(test_loss, label='Тестовая выборка')
-axs[1].plot(train_loss, label='Обучающая выборка')
+axs[1].plot(test_epochs, test_loss, label='Тестовая выборка')
+axs[1].plot(train_epochs, train_loss, label='Обучающая выборка')
 axs[1].set_xlabel('Эпоха')
 axs[1].set_ylabel('Функция потерь')
 axs[1].legend()
