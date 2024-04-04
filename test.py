ff4b035a327fed062705b8fa8eff107ca3cf8c7f
Roundabout1 <mrsivannap@gmail.com>
2024-04-04 18:40:27 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ from torchvision import datasets, transforms
 from FCnet import FCnet
 from tester import NetTester
 from data_load import balancing
-
 from customSGD import CustomSGD
+from loss import Loss, Loss_L2
 OUTPUT_ROOT = "output"
 
 device = (
@@ -23,11 +23,10 @@ print(f"Using {device} device")
 train_num = 60000
 train_dataset = datasets.MNIST(root='./data', train=True, transform=transforms.ToTensor(), download=True)
 test_dataset = datasets.MNIST(root='./data', train=False, transform=transforms.ToTensor(), download=True)
-
-batch_size = 128
-
-train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+train_num = 100
+train_dataset = balancing(train_dataset, 10, train_num)
+train_dataloader = DataLoader(train_dataset, batch_size=25, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size=128, shuffle=True)
 
 for X, y in test_dataloader:
     print(f"Shape of X [N, C, H, W]: {X.shape}")
@@ -41,9 +40,12 @@ tester = NetTester(
       train_dataloader=train_dataloader,
       test_dataloader=test_dataloader,
       optimizer=CustomSGD(model.parameters(), lr=0.05),
-      loss_fn=nn.CrossEntropyLoss()
+      loss=Loss_L2(loss_fn=nn.CrossEntropyLoss(), model_parameters=model.parameters(), l2_lambda=0.01, ignore_bias=True)
 )
 
-tester.train(epochs=5*(60000//train_num))
+tester.train(
+    epochs=60*(60000//train_num), 
+    dont_skip=200,
+    test_every=50         )
 
 tester.save_results(OUTPUT_ROOT)
