d097a87f68861ab4e0643d434c51616629f07403
Roundabout1 <mrsivannap@gmail.com>
2024-05-05 22:03:27 +0800

    get_device изменение

--- main.py
+++ main.py
@@ -11,21 +11,25 @@ from loss import Loss, Loss_L2
 from SMD_opt import SMD_qnorm
 OUTPUT_ROOT = "output"
 
-device = (
-    "cuda"
-    if torch.cuda.is_available()
-    else "mps"
-    if torch.backends.mps.is_available()
-    else "cpu"
-)
-print(f"Using {device} device")
+def get_device():
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+            else "mps"
+        if torch.backends.mps.is_available()
+            else "cpu"
+    )
+    print(f"Using {device} device")
+    return device
+
+device = get_device()
 
 def make_models(num_models, model):
     """
     создать num_models одинаковых моделей
     """
-    models = [model]
-    for i in range(num_models-1):
+    models = []
+    for i in range(num_models):
         cur_model = model.clone().to(device)
         models.append(cur_model)
     return models
@@ -36,7 +40,10 @@ def make_dataloaders(num_loaders, dataset, dataloader_size, batch_size, lables_n
     """
     dataloaders = []
     for i in range(num_loaders):
-        balanced_dataset = balancing(dataset, lables_num, dataloader_size)
+        if dataloader_size is not None:
+            balanced_dataset = balancing(dataset, lables_num, dataloader_size)
+        else:
+            balanced_dataset = dataset
         dataloader = DataLoader(balanced_dataset, batch_size=batch_size, shuffle=True)
         dataloaders.append(dataloader)
     return dataloaders
@@ -136,30 +143,3 @@ for i in range(num_testers):
 run_tests(testers, 20, 5, 2)
 main_tester = concat_results(testers)
 main_tester.save_results(OUTPUT_ROOT, 'test')
-""" train_num = 100
-train_dataset = balancing(train_dataset, 10, train_num)
-train_dataloader = DataLoader(train_dataset, batch_size=25, shuffle=True)
-test_dataloader = DataLoader(test_dataset, batch_size=128, shuffle=True)
-
-for X, y in test_dataloader:
-    print(f"Shape of X [N, C, H, W]: {X.shape}")
-    print(f"Shape of y: {y.shape} {y.dtype}")
-    break
-
-model = FCnet(800, 28*28, 10, 2).to(device)
-tester = NetTester(
-      model=model,
-      device=device,
-      train_dataloader=train_dataloader,
-      test_dataloader=test_dataloader,
-      optimizer=optim.SGD(model.parameters(), lr=0.05),
-      loss=Loss_L2(loss_fn=nn.CrossEntropyLoss(), model_parameters=model.parameters(), l2_lambda=0.01)
-)
-
-tester.train(
-    epochs=60*(60000//train_num), 
-    dont_skip=100,
-    test_every=100,
-)
-
-tester.save_results(OUTPUT_ROOT) """
