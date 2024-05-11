c4da7c7aa791f63062fe63ccf0efbb523e07a033
Roundabout1 <mrsivannap@gmail.com>
2024-05-12 00:23:25 +0800

    удаление ненужного вывода

--- customSGD.py
+++ customSGD.py
@@ -3,10 +3,6 @@ from torch.optim.optimizer import Optimizer
 class CustomSGD(Optimizer):
     def __init__(self, params, lr=1e-3):
         super(CustomSGD, self).__init__(params, defaults={'lr': lr})
-        for group in self.param_groups: 
-            for p in group['params']:
-                print(p.shape)
-
 
     def step(self): 
         for group in self.param_groups:
